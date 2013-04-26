39c0c7923aab35b6518ac72258593967cd839d33
gipsaliu@gmail.com <gipsaliu@gmail.com@c53e62a7-6341-69ac-adb8-d6373e73d353>
2013-04-26 15:51:01 +0000

    改进了页面显示

--- analyse.php
+++ analyse.php
@@ -4,9 +4,35 @@ $basefilename = basename(__FILE__);
 $h2 = substr($basefilename, 0, strpos($basefilename, '.'));
 
 ini_set("display_errors", 1);
-require('includes/functions/analyse.php');	// todo modify analyse.php and require it
+require('includes/functions/analyse.php');
 if(isset($_GET['start_analyse'])) {
 // todo: analyse
+	//if(!empty($_GET['captcha-source'])) {$captcha_source = true;} else {$rgba_flag = false; }
+	if(empty($_GET['captcha-source'])) {
+		$err_msgs[] = 'did not choose captcha type';
+	} else {
+		$captcha_source = $_GET['captcha-source'];
+	}
+	if(empty($_GET['binarize-parameter-rgba'])) {
+		$err_msgs[] = 'did not choose binarize parameter 1';
+	} else {
+		$binarize_parameters['rgba'] = $_GET['binarize-parameter-rgba'];
+	}
+	if(empty($_GET['binarize-parameter-relation'])) {
+		$err_msgs[] = 'did not choose binarize parameter 2';
+	} else {
+		$binarize_parameters['relation'] = $_GET['binarize-parameter-relation'];
+	}
+	if(empty($_GET['binarize-parameter-value'])) {
+		$err_msgs[] = 'did not choose binarize parameter 3';
+	} else {
+		$binarize_parameters['value'] = $_GET['binarize-parameter-value'];
+	}
+
+	if(!empty($_GET['process-outputrgba'])) {$rgba_flag = true;} else {$rgba_flag = false; }
+	if(!empty($_GET['process-dropnoise'])) {$dropnoise_flag = true;} else {$dropnoise_flag = false; }
+	if(!empty($_GET['process-divide'])) {$divide_flag = true;} else {$divide_flag = false; }
+	if(!empty($_GET['process-align'])) {$align_flag = true;} else {$align_flag = false; }
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -51,17 +77,14 @@ if(isset($_GET['start_analyse'])) {
 	<select name="binarize-parameter-value">
 		<?php
 		for($i=0; $i < 256; $i++) {
-			if(66==$i) {
-				echo "<option value=\"$i\" selected=\"selected\" >$i</option>";
-			} else {
-				echo "<option value=\"$i\">$i</option>";
-			}
+			if(66==$i) { echo "<option value=\"$i\" selected=\"selected\" >$i</option>";
+			} else { echo "<option value=\"$i\">$i</option>"; }
 		}
 		?>
 	</select>
 
 	<br />
-	请选择<u><em>分析过程：</em></u><br />
+	请选择<u><em>分析过程</em></u>：<br />
 	<label for="process-0"><input type="checkbox" name="process-outputrgba" value="true" id="process-0" <?php if(!empty($_GET['process-outputrgba'])){echo 'checked="checked"';} ?> />输出rgb信息</label>&nbsp;&nbsp;&nbsp;
 	<label for="process-1"><input type="checkbox" name="process-binarize" value="true" id="process-1" checked="checked" disabled="disabled" />二值化(必选)</label>&nbsp;&nbsp;&nbsp;
 	<label for="process-2"><input type="checkbox" name="process-dropnoise" value="true" id="process-2" <?php if(!empty($_GET['process-dropnoise'])) {echo 'checked="checked"';} ?> />降噪</label>&nbsp;&nbsp;&nbsp;
@@ -73,15 +96,38 @@ if(isset($_GET['start_analyse'])) {
 
 	<div id="main-content">
 	<?php
-	if(isset($_GET['start_analyse'])) {
-		if(!empty($_GET['process-outputrgba'])) {$rgba_flag = true;} else {$rgba_flag = false; }
-		if(!empty($_GET['process-dropnoise'])) {$dropnoise_flag = true;} else {$dropnoise_flag = false; }
-		if(!empty($_GET['process-divide'])) {$divide_flag = true;} else {$divide_flag = false; }
-		if(!empty($_GET['process-align'])) {$align_flag = true;} else {$align_flag = false; }
-		echo '<pre>';
-		//var_dump($_GET);
-		analysebyfile('./images/test/pps/0.png', 'jpg', 66, '==', $rgba_flag, $dropnoise_flag, $divide_flag, $align_flag);
-		echo '</pre>';
+	if(!empty($err_msgs)) {
+		foreach($err_msgs as $err_msg) {
+			echo '<p class="error">'.$err_msg.'</p>';
+		}
+	} else if(isset($_GET['start_analyse'])) {
+		switch ($_GET['captcha-source']) {
+			case 'pps':
+				$dir = './images/analyse/pps/';
+				$type = 'png';
+				break;
+			case 'njaumy';
+				$dir = './images/analyse/njaumy/';
+				$type = 'jpg';
+				break;
+			case 'phpwind';
+				$dir = './images/analyse/phpwind/';
+				$type = 'png';
+				break;
+			default :
+				break;
+		}
+		$files = glob($dir.'*.'.$type);
+		if(0 == count($files)) {
+			echo '<p class="error">no specified files found! please contact gipsa</p>';
+			exit();
+		}
+		foreach($files as $file ) {
+			echo "<h3>$file</h3>";
+			echo '<pre>';
+			analysebyfile($file, $type, $binarize_parameters, $rgba_flag, $dropnoise_flag, $divide_flag, $align_flag);
+			echo '</pre>';
+		}
 	}
 	?>
 	<p>here is main-content</p>
