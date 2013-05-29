092e9560386d04f772fe56502d8282469b94fe60
gipsaliu@gmail.com <gipsaliu@gmail.com@c53e62a7-6341-69ac-adb8-d6373e73d353>
2013-05-29 01:42:55 +0000

    delete the result dir

--- result/index.inc.php
+++
@@ -1,8 +0,0 @@
-	<div id="index">
-	<ul class="">
-	<li><a href="result-pps.php">pps</a></li>
-	<li><a href="result-njaumy.php">njaumy</a></li>
-	<li><a href="result-pps.php?on-line=true">pps-online</a></li>
-	<li><a href="result-njaumy.php?on-line=true">njaumy-online</a></li>
-	</ul>
-	</div>
